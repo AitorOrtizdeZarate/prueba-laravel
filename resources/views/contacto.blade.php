552108b35becb2912e6d726d3cb36dddbfe69d72
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-10-30 09:38:01 +0100

    4.1 a) ruta contactos hecho

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contactos</title>
+</head>
+<body>
+	<h1>Contactos</h1>
+	<p><b>Nombre:</b> Aitor Ortiz de Zarate</p>
+	<p><b>Edad:</b> 19 años</p>
+	<p><b>Localidad:</b> Lezo</p>
+
+</body>
+</html>
